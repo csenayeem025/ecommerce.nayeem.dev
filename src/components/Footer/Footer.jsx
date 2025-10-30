b57c74d483099c9471fc81a432e06c3b41c79410
Md. Khurshed Alam Nayem <csenayeem025@gmail.com>
2025-10-30 22:39:03 +0600

    Initial Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -28,36 +28,20 @@ export default function Footer() {
                     Other Sites
                   </h3>
                   <a
-                    href="https://mohamedemary.github.io/route-frontend/assignments/10-daniels/index.html"
+                    href="https://www.nayeem.dev"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
                   >
-                    Daniels
+                    Nayeem.dev
                   </a>
                   <a
-                    href="https://mohamedemary.github.io/route-frontend/assignments/07-devFolio/index.html"
+                    href="https://www.anavasoft.com"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
                   >
-                    Devfolio
-                  </a>
-                  <a
-                    href="https://game-over-ivory.vercel.app/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                  >
-                    Game Over
-                  </a>
-                  <a
-                    href="https://todo-app-wine-tau.vercel.app/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline"
-                  >
-                    Todo App
+                    AnavaSoft
                   </a>
                 </div>
                 <div>
@@ -120,49 +104,30 @@ export default function Footer() {
                   </h3>
                   <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">
                     <a href="mailto:mohamed.ahmed.emary@gmail.com">
-                      mohamed.ahmed.emary@gmail.com
+                      csenayeem025@gmail.com
                     </a>
                   </span>
                   <span className="block space-x-2 mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">
                     <a
-                      href="https://linkedin.com/in/mohamedemary"
+                      href="https://linkedin.com/in/csenayeem025"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
                       <i className="fa-lg fa-fw  fab fa-linkedin"></i>
                     </a>
                     <a
-                      href="https://github.com/mohamedemary"
+                      href="https://github.com/csenayeem025"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
                       <i className="fa-lg fa-fw  fab fa-github"></i>
                     </a>
                     <a
-                      href="mailto:mohamed.ahmed.emary@gmail.com"
+                      href="mailto:csenayeem025@gmail.com"
                       target="_blank"
                       rel="noopener noreferrer"
                     >
                       <i className="fa-lg fa-fw  far fa-envelope"></i>
-                    </a>
-                    <a
-                      href="https://leetcode.com/Spark71"
-                      className="inline"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <svg
-                        className="inline text-sm"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          fill="currentColor"
-                          d="M13.483 0a1.37 1.37 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.3 5.3 0 0 0-1.209 2.104a5 5 0 0 0-.125.513a5.5 5.5 0 0 0 .062 2.362a6 6 0 0 0 .349 1.017a5.9 5.9 0 0 0 1.271 1.818l4.277 4.193l.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.38 1.38 0 0 0-1.951-.003l-2.396 2.392a3.02 3.02 0 0 1-4.205.038l-.02-.019l-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.7 2.7 0 0 1 .066-.523a2.55 2.55 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0m-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382a1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382a1.38 1.38 0 0 0-1.38-1.382z"
-                        />
-                      </svg>
                     </a>
                   </span>
                 </div>
@@ -178,7 +143,7 @@ export default function Footer() {
           </div>
           <div>
             <div className="text-center italic py-3 text-gray-500 dark:text-gray-400">
-              Made with love and passion by Mohamed Emary
+              Made with love and passion by Md. Khurshed Alam Nayem
             </div>
           </div>
         </div>
